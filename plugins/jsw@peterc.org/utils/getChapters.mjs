5d912e7adeaf01d17d041d6ea106e6f35d2c0916
Anton Nemtsev <thesilentimp@gmail.com>
2020-05-13 23:09:07 +0200

    added plugin for JavaScript Weekly

---
+++ plugins/jsw@peterc.org/utils/getChapters.mjs
@@ -0,0 +1,16 @@
+import getFromTo from '../../../utils/getFromTo.mjs';
+
+const parts = [
+'JavaScript Weekly', // from 0
+// part 1
+'Jobs', // to 1
+// skipped
+'Tutorials', // from 2
+// part 2
+'Code & Tools', // to 3
+];
+
+export default (text) => [
+  getFromTo(parts[0], parts[1], text), 
+  getFromTo(parts[2], parts[3], text)
+].join('\n\n');
--- utils/banVendors.mjs
+++ utils/banVendors.mjs
@@ -1,3 +1,3 @@
 import filterVendorLinks from './filterVendorLinks.mjs';
-const bannedVendors = ['twitter.com','codepen.io','facebook.com','smashingconf.com','github.com'];
+const bannedVendors = ['twitter.com','codepen.io','facebook.com','smashingconf.com','github.com','youtube.com'];
 export default (links) => bannedVendors.reduce((links, vendor) =>  filterVendorLinks(vendor)(links), links);
